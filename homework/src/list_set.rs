1c2a81807fa49c194f2fa95ff527ded12bdb41df
integerblue <chlwjdtn1120@gmail.com>
2020-11-04 15:05:53 +0000

    Lock-based Linked List Implemented without drop

--- homework/src/list_set.rs
+++ homework/src/list_set.rs
@@ -37,7 +37,21 @@ impl<'l, T: Ord> Cursor<'l, T> {
     /// Move the cursor to the position of key in the sorted list. If the key is found in the list,
     /// return `true`.
     fn find(&mut self, key: &T) -> bool {
-        todo!()
+        let ptr = *self.0;
+        if ptr.is_null() {
+            return false;
+        }
+
+        let node = unsafe { ptr.as_ref().unwrap() };
+
+        if node.data == *key {
+            true
+        } else if node.data > *key {
+            false
+        } else {
+            self.0 = node.next.lock().unwrap();
+            Self::find(self, key)
+        }
     }
 }
 
@@ -52,22 +66,65 @@ impl<T> OrderedListSet<T> {
 
 impl<T: Ord> OrderedListSet<T> {
     fn find(&self, key: &T) -> (bool, Cursor<T>) {
-        todo!()
+        let mut cursor = Cursor(self.head.lock().unwrap());
+        let result = cursor.find(key);
+        (result, cursor)
     }
 
     /// Returns `true` if the set contains the key.
     pub fn contains(&self, key: &T) -> bool {
-        todo!()
+        let (result, _) = self.find(key);
+        result
     }
 
     /// Insert a key to the set. If the set already has the key, return the provided key in `Err`.
     pub fn insert(&self, key: T) -> Result<(), T> {
-        todo!()
+        unsafe {
+            let mut prev_guard = self.head.lock().unwrap();
+            let mut next_guard = prev_guard;
+            loop {
+                if let Some(node) = (*next_guard).as_ref() {
+                    let data = &node.data;
+                    if *data > key {
+                        *next_guard = Node::new(key, *next_guard);
+                        return Ok(());
+                    } else if *data < key {
+                        prev_guard = next_guard;
+                        next_guard = (*(*prev_guard)).next.lock().unwrap();
+                    } else {
+                        return Err(key);
+                    }
+                } else {
+                    *next_guard = Node::new(key, *next_guard);
+                    return Ok(());
+                }
+            }
+        }
     }
 
     /// Remove the key from the set and return it.
     pub fn remove(&self, key: &T) -> Result<T, ()> {
-        todo!()
+        unsafe {
+            let mut prev_guard = self.head.lock().unwrap();
+            let mut next_guard = prev_guard;
+            loop {
+                if let Some(node) = (*next_guard).as_ref() {
+                    let data = &node.data;
+                    if *data > *key {
+                        return Err(());
+                    } else if *data < *key {
+                        prev_guard = next_guard;
+                        next_guard = (*(*prev_guard)).next.lock().unwrap();
+                    } else {
+                        let node = Box::from_raw(*next_guard);
+                        *next_guard = *node.next.lock().unwrap();
+                        return Ok(node.data);
+                    }
+                } else {
+                    return Err(());
+                }
+            }
+        }
     }
 }
 
@@ -85,13 +142,21 @@ impl<'l, T> Iterator for Iter<'l, T> {
     type Item = &'l T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        let guard = self.0.as_ref().unwrap();
+        if let Some(node) = unsafe { (*guard).as_ref() } {
+            let next_guard = node.next.lock().unwrap();
+            self.0 = Some(next_guard);
+            Some(&node.data)
+        } else {
+            self.0 = None;
+            None
+        }
     }
 }
 
 impl<T> Drop for OrderedListSet<T> {
     fn drop(&mut self) {
-        todo!()
+        // todo!()
     }
 }
 
